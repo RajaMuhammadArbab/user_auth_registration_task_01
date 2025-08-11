79a4169ab326e4777e4bac20d9dd65fea0613c14
Raja Muhammad Arbab <rajaarbabthakur1@gmail.com>
2025-08-11 11:00:27 +0500

    Update settings.py

--- user_auth_registration/settings.py
+++ user_auth_registration/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b(ystxmqg7ou1u^jvf643p=spx!8&j@=bswm%_7p(9(k3gsj(2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = false
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['RajaMuhammadArbab.pythonanywhere.com']
 
 
 # Application definition
